bd4b10e48d69aeb42b7a8c20019cf84d01f1cb26
curiousmustafa <mi0117634@gmail.com>
2021-09-29 13:56:35 +0200

    Added CompositionLocalProvider

--- app/src/main/java/com/example/muzicx/viewmodel/ParentViewModel.kt
+++ app/src/main/java/com/example/muzicx/viewmodel/ParentViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.muzicx.model.MyTrack
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class ParentViewModel : ViewModel(){
     // the song that to be played in player screen
     private var _track = MutableLiveData<MyTrack>()
